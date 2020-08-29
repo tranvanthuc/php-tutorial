c21b2c4b546d02e7a935ac7d2ad24b19bf4adbc0
Tran Van Thuc <tranvanthucduytan@gmail.com>
2020-08-29 20:43:01 +0700

    Create form with method GET

---
+++ index.view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Form</title>
+</head>
+
+<body>
+    <form action="server.php" method="get">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = $_GET["name"];
+$email = $_GET["email"];
+
+echo "Name: $name <br>";
+echo "Email: $email <br>";
