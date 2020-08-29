bd52219defa3e4162159876fc25918f91f64ac2e
Tran Van Thuc <tranvanthucduytan@gmail.com>
2020-08-29 20:58:27 +0700

    Create form with method POST

---
+++ index.view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form</title>
+</head>
+<body>
+    <form action="server.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,7 @@
+<?php 
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+
+echo "Name: $name <br>";
+echo "Email: $email <br>";
