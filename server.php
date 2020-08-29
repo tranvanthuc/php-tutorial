b59823c4a6950e5659395c99afa0c97377b65006
Tran Van Thuc <tranvanthucduytan@gmail.com>
2020-08-29 21:02:42 +0700

    Add validate for form

--- server.php
+++ server.php
@@ -3,5 +3,9 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 
-echo "Name: $name <br>";
-echo "Email: $email <br>";
+if (trim($name) == '' || trim($email) == '') {
+    echo "Please enter name and email!";
+} else {
+    echo "Name: $name <br>";
+    echo "Email: $email <br>";
+}
