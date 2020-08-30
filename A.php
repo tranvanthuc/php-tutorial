69db0fa80b0536df4fe149f490f67b4c66e3ff4e
Tran Van Thuc <tranvanthucduytan@gmail.com>
2020-08-30 17:33:41 +0700

    Use Setter Injection

--- A.php
+++ A.php
@@ -7,15 +7,16 @@ class A
     // thuộc tính $b của class A
     public $b;
 
-    public function __construct($bInstance)
-    {
-        $this->b = $bInstance;
-    }
-
     // phương thức showMessage của class A
     public function showMessage()
     {
         $message = $this->b->getMessage();
         echo $message;
+    }
+
+    // thực hiện setter injection
+    public function setB($bInstance)
+    {
+        $this->b = $bInstance;
     }
 }
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 require "A.php";
 
 $b = new B("This is DI messsage");
-$a = new A($b);
+$a = new A;
 
+// gọi setB để set instance B vào thuộc tinh $b của class A
+$a->setB($b);
 $a->showMessage();
